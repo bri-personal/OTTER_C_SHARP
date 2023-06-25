e9a482927c81ab9110165fd40458e8e957e6b5e3
bri-personal <107887789+bri-personal@users.noreply.github.com>
2023-06-25 13:59:00 -0700

    Add project files.

---
+++ OTTER_C_SHARP/Components.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+
+namespace Otter
+{
+    internal interface Component
+    {
+        void Update();
+    }
+    internal class CompPC : Component
+    {
+        private BitArray reset;
+        private BitArray pcWrite;
+        private BitArray clock;
+        private BitArray input;
+        private BitArray output;
+
+        public CompPC(BitArray reset, BitArray pcWrite, BitArray input, BitArray output, BitArray clock)
+        {
+            this.reset = reset;
+            this.pcWrite = pcWrite;
+            this.clock = clock;
+            this.input = input;
+            this.output = output;
+        }
+
+        public void Update()
+        {
+            if (clock[0])
+            {
+                if (reset[0])
+                {
+                    output.SetAll(false);
+                }
+                else if (pcWrite[0])
+                {
+                    for(int i = 0; i<output.Length; i++)
+                    {
+                        output.Set(i, input[i]);
+                    }
+                }
+            }
+        }
+        
+    }
+    /*
+    internal class CompMUX : Component
+    {
+        UInt32 * [] inputs;
+        UInt32 * output;
+        UInt32 * sel;
+
+        public CompMUX(UInt32* []inputs, UInt32 *output, UInt32 *sel)
+        {
+            this.inputs = inputs;
+            this.output = output;
+            this.sel = sel;
+            Console.WriteLine((int)(&inputs));
+        }
+
+        public void Update()
+        {
+            *output = *inputs[*sel];
+        }
+    }
+    */
+}
---
+++ OTTER_C_SHARP/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+
+namespace Otter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MCU otter = new MCU();
+        }
+    }
+    
+    public unsafe class MCU
+    {
+        //components
+        CompPC pc;
+        //CompMUX pcMux;
+
+        /*signals*/
+        BitArray clock;
+
+        //PC
+        BitArray reset, pcWrite, pcIn, pcOut;
+
+        //PC MUX
+        BitArray pc4, jalr, branch, jal, mtvec, mepc, pcSource;
+
+        public MCU()
+        {
+            /*initialize signals*/
+            clock = new BitArray(1);
+
+            //PC
+            reset = new BitArray(1);
+            pcWrite = new BitArray(1);
+            pcIn = new BitArray(32);
+            pcOut = new BitArray(32);
+
+            //PC MUX
+            pc4 = new BitArray(32);
+            jalr = new BitArray(32);
+            branch = new BitArray(32);
+            jal = new BitArray(32);
+            mtvec = new BitArray(32);
+            mepc = new BitArray(32);
+            pcSource = new BitArray(3);
+
+
+            /*initialize components*/
+            CompPC pc = new CompPC(reset, pcWrite, pcIn, pcOut, clock);
+            clock[0] = true;
+            pcIn[0] = true;
+            pcWrite[0] = true;
+            Console.WriteLine(pcOut[0]);
+            pc.Update();
+            Console.WriteLine(pcOut[0]);
+            reset[0] = true;
+            pc.Update();
+            Console.WriteLine(pcOut[0]);
+        }
+    }
+}
