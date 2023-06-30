a02fa41840fd1a00f9944040572a737772c94a18
bri-personal <107887789+bri-personal@users.noreply.github.com>
2023-06-29 17:15:49 -0700

    total rework - fields now condensed to single class that reads binary file and parses for instruction type

--- OTTER_C_SHARP/Program.cs
+++ OTTER_C_SHARP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
 
 namespace Otter
 {
@@ -7,167 +8,342 @@ namespace Otter
         public static void Main(string[] args)
         {
             MCU otter = new MCU(); //create OTTER object
-
-            /*
-            BitArray b = new BitArray(5);
-            BitArray c= new BitArray(5, true);
-            Console.WriteLine("{0} {1}",Util.BitArrayToInt32(b), Util.BitArrayToInt32(c));
-            Util.CopyBits(b, c, 1, 3);
-            Console.WriteLine("{0} {1}", Util.BitArrayToInt32(b), Util.BitArrayToInt32(c));
-            Util.CopyBits(c, b, destStart: 4);
-            Console.WriteLine("{0} {1}", Util.BitArrayToInt32(b), Util.BitArrayToInt32(c));
-            */
         }
     }
-    
+
     public class MCU
     {
-        //components
-        CompPC pc;
-        CompMUX pcMux;
-        CompMemory memory;
-        CompRegFile regFile;
-        CompMUX regFileMux;
-        Component[] components;
+        //bitmask constants
+        private const Int32 OPCODE_MASK = 0x7F;
+        private const Int32 FUNC3_MASK = 0x00007000;
 
-        /*signals*/
-        BitArray clock;
+        //opcode constants
+        private const byte LUI_OPCODE = 0x37;
+        private const byte AUIPC_OPCODE = 0x17;
+        private const byte JAL_OPCODE = 0x6F;
+        private const byte JALR_OPCODE = 0x67;
+        private const byte L_OPCODE = 0x03;
+        private const byte I_OPCODE = 0x13;
+        private const byte B_OPCODE = 0x63;
+        private const byte S_OPCODE = 0x23;
+        private const byte R_OPCODE = 0x33;
+        private const byte SYS_OPCODE = 0x73;
 
-        //PC
-        BitArray reset, pcWrite, pcIn, pcOut;
+        //external IO
+        private bool RST, INTR, CLK, IOBUS_WR;
+        private Int32 IOBUS_IN, IOBUS_OUT, IOBUS_ADDR;
 
-        //PC MUX
-        BitArray pc4, jalr, branch, jal, mtvec, mepc, pcSource;
+        //internal components
+        private Int32 pc; //program count
+        private Int32[] regs; //data registers - length 32 array of length 32 BitArrays
+        Int32 ir; //array to hold instruction read from text segment file
 
-        //Memory
-        BitArray memRdEn1, memRdEn2, memWE2, ir, result, memOut2, iobusIn, ioWr;
-
-        //Reg File and Mux
-        BitArray regWrite, regWD, rs1Out, rs2Out, rfWrSel, csrRD;
+        FileStream text; //file for text segment of memory
+        BinaryReader textReader; //reader for text segment of memory
 
         public MCU()
         {
-            /*initialize signals*/
-            clock = new BitArray(1);
+            pc = 0; //pc starts at 0
 
-            //PC
-            reset = new BitArray(1);
-            pcWrite = new BitArray(1);
-            pcIn = new BitArray(32);
-            pcOut = new BitArray(32);
+            //initialize registers
+            regs = new Int32[32];
+            for (int i = 0; i < 32; i++)
+            {
+                regs[i] = 0;
+            }
 
-            //PC MUX
-            pc4 = new BitArray(32);
-            jalr = new BitArray(32);
-            branch = new BitArray(32);
-            jal = new BitArray(32);
-            mtvec = new BitArray(32);
-            mepc = new BitArray(32);
-            pcSource = new BitArray(3);
-
-            //Memory
-            memRdEn1 = new BitArray(1);
-            memRdEn2 = new BitArray(1);
-            memWE2 = new BitArray(1);
-            ir = new BitArray(32);
-            result = new BitArray(32);
-            memOut2 = new BitArray(32);
-            iobusIn = new BitArray(32);
-            ioWr = new BitArray(1);
-
-            //Reg File
-            regWrite=new BitArray(1);
-            regWD = new BitArray(32);
-            rs1Out = new BitArray(32);
-            rs2Out = new BitArray(32);
-            csrRD= new BitArray(32);
-            rfWrSel = new BitArray(1);
-
-
-            /*initialize components*/
-            pc = new CompPC(reset, pcWrite, pcIn, pcOut, clock);
-            pcMux = new CompMUX(new BitArray[6] {pc4, jalr, branch, jal, mtvec, mepc}, pcIn, pcSource);
-            memory = new CompMemory(memRdEn1, memRdEn2, memWE2, clock, pcOut, result, rs2Out, ir, iobusIn, memOut2, ioWr);
-            regFile = new CompRegFile(ir, clock, regWD, regWrite, rs1Out, rs2Out);
-            regFileMux = new CompMUX(new BitArray[] { pc4, csrRD, memOut2, result }, regWD, rfWrSel);
-            components = new Component[] { pcMux, pc, memory, regFile, regFileMux }; //array to iterate through to update
-
-
-            //testing
-            clock[0] = true;
-
-            /*
-            Console.WriteLine("PC IN: {0}\nPC OUT: {1}\n", Util.BitArrayToInt32(pcIn), Util.BitArrayToInt32(pcOut));
-            pcSource[0] = true; //pcSource=1
-            jalr[0] = true; //jalr=1
-            UpdateAll(); //mux out=1
-            Console.WriteLine("PC IN: {0}\nPC OUT: {1}\n", Util.BitArrayToInt32(pcIn), Util.BitArrayToInt32(pcOut));
-
-            pcWrite[0] = true; //pc write=on
-            UpdateAll(); //pc out = jalr = 1
-            Console.WriteLine("PC IN: {0}\nPC OUT: {1}\n", Util.BitArrayToInt32(pcIn), Util.BitArrayToInt32(pcOut));
-            PrintMem();
-
-            memWE2[0]= true; //memory write=on
-            rs2Out.SetAll(true); //memory din2 set to 1s
-            result[0]=true; //memory addr2 = 1
-            ir[14] = true; //unsigned (size=8)
-            UpdateAll(); //memory is 8 '0's followed by 8 '1's
-            PrintMem();
-
-            memWE2[0] = false; //memory write=off
-            memRdEn2[0] = true; //memory read2 = on
-            UpdateAll(); //memout2 is updated
-            Console.WriteLine("Memory Out 2: {0}", Util.BitArrayToInt32(memOut2));
-
-            ir[12] = false; //reset size to not interfere
-            ir[14] = false; //reset sign to not interfere
-            memRdEn1[0] = true; //memory read1 = on
-            memRdEn2[0] = false; //memory read2 = off
-            UpdateAll(); //ir is updated
-            Console.WriteLine("Memory Out 1: {0}", Util.BitArrayToInt32(ir));
-            */
-            /*
-            Console.WriteLine("RegFile reg 1 before:\n{0}", Util.BitsToString(regFile.Regs[1]));
-            regWD[0] = true; //write data =1
-            ir[7] = true; //wa =1
-            regWrite[0] = true; //regfile write enable= on
-            UpdateAll(); //1 written to register 1
-            Console.WriteLine("RegFile reg 1 written:\n{0}", Util.BitsToString(regFile.Regs[1]));
-
-            regWrite[0] = false; //regfile write = off
-            ir[15] = true; //adr1 = 1
-            ir[20] = true; //adr2 =1
-            UpdateAll(); //adr1 and adr2 show 1
-            Console.WriteLine("RS1:\n{0}", Util.BitsToString(rs1Out));
-            Console.WriteLine("RS2:\n{0}", Util.BitsToString(rs2Out));
-            */
+            Run();
         }
 
-        public void UpdateAll()
+        public void Run()
         {
-            //call Update for all components
-            foreach(Component c in components)
+            using (text = File.Open("otter_memory.mem", FileMode.Open, FileAccess.Read))
             {
-                c.Update();
-
-                if(c==pc)
+                using (textReader = new BinaryReader(text))
                 {
-                    //add 4 to pc4
-                    Util.SetBitArrayToInt32(pc4, Util.BitArrayToInt32(pcOut) + 4);
+                    while (pc <= 0x2044)
+                    {
+                        Console.Write(Convert.ToString(pc, 16) + ": ");
+                        LoadInstruction();
+                        Console.Write(Convert.ToString(ir, 16).PadLeft(8, '0')+" ");
+                        ParseInstruction();
+                    }
                 }
             }
-
         }
 
-        public void PrintMem()
+        private void LoadInstruction()
         {
-            Console.WriteLine("Memory:");
-            for (int i = 0; i < 25; i++)
+            text.Seek(pc, SeekOrigin.Begin);
+            ir = textReader.ReadInt32();
+            pc = (Int32)text.Seek(0, SeekOrigin.Current);
+        }
+
+        private void ParseInstruction()
+        {
+            switch (ir & OPCODE_MASK)
             {
-                Console.Write("{0} ", memory.Mem[i] ? 1 : 0);
+                case LUI_OPCODE:
+                    {
+                        Console.WriteLine("lui");
+                        break;
+                    }
+                case AUIPC_OPCODE:
+                    {
+                        Console.WriteLine("auipc");
+                        break;
+                    }
+                case JAL_OPCODE:
+                    {
+                        Console.WriteLine("jal");
+                        break;
+                    }
+                case JALR_OPCODE:
+                    {
+                        Console.WriteLine("jalr");
+                        break;
+                    }
+                case L_OPCODE:
+                    {
+                        Console.Write("l");
+                        switch ((ir & FUNC3_MASK) >> 12)
+                        {
+                            case 0:
+                                {
+                                    Console.WriteLine("b");
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    Console.WriteLine("h");
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Console.WriteLine("w");
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    Console.WriteLine("bu");
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    Console.WriteLine("hu");
+                                    break;
+                                }
+                            default:
+                                {
+                                    Console.WriteLine("UNKNOWN");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case I_OPCODE:
+                    {
+                        switch ((ir & FUNC3_MASK) >> 12)
+                        {
+                            case 0:
+                                {
+                                    Console.WriteLine("addi");
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    Console.WriteLine("slli");
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Console.WriteLine("slti");
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    Console.WriteLine("sltiu");
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    Console.WriteLine("xori");
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    Console.WriteLine("srli/srai");
+                                    break;
+                                }
+                            case 6:
+                                {
+                                    Console.WriteLine("ori");
+                                    break;
+                                }
+                            default: //7 - only other option
+                                {
+                                    Console.WriteLine("andi");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case B_OPCODE:
+                    {
+                        Console.Write("b");
+                        switch((ir&FUNC3_MASK)>>12)
+                        {
+                            case 0:
+                                {
+                                    Console.WriteLine("eq");
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    Console.WriteLine("ne");
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    Console.WriteLine("lt");
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    Console.WriteLine("ge");
+                                    break;
+                                }
+                            case 6:
+                                {
+                                    Console.WriteLine("ltu");
+                                    break;
+                                }
+                            case 7:
+                                {
+                                    Console.WriteLine("geu");
+                                    break;
+                                }
+                            default:
+                                {
+                                    Console.WriteLine("UNKNOWN");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case S_OPCODE:
+                    {
+                        Console.Write("s");
+                        switch ((ir & FUNC3_MASK) >> 12)
+                        {
+                            case 0:
+                                {
+                                    Console.WriteLine("b");
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    Console.WriteLine("h");
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Console.WriteLine("w");
+                                    break;
+                                }
+                            default:
+                                {
+                                    Console.WriteLine("UNKNOWN");
+                                    break;
+                                }
+                        }
+                                break;
+                    }
+                case R_OPCODE:
+                    {
+                        switch ((ir & FUNC3_MASK) >> 12)
+                        {
+                            case 0:
+                                {
+                                    Console.WriteLine("add/sub");
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    Console.WriteLine("sll");
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Console.WriteLine("slt");
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    Console.WriteLine("sltu");
+                                    break;
+                                }
+                            case 4:
+                                {
+                                    Console.WriteLine("xor");
+                                    break;
+                                }
+                            case 5:
+                                {
+                                    Console.WriteLine("srl/sra");
+                                    break;
+                                }
+                            case 6:
+                                {
+                                    Console.WriteLine("or");
+                                    break;
+                                }
+                            default: //7 - only other option
+                                {
+                                    Console.WriteLine("and");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                case SYS_OPCODE:
+                    {
+                        switch ((ir & FUNC3_MASK) >> 12)
+                        {
+                            case 0:
+                                {
+                                    Console.WriteLine("mret");
+                                    break;
+                                }
+                            case 1:
+                                {
+                                    Console.WriteLine("csrrw");
+                                    break;
+                                }
+                            case 2:
+                                {
+                                    Console.WriteLine("csrrs");
+                                    break;
+                                }
+                            case 3:
+                                {
+                                    Console.WriteLine("csrrc");
+                                    break;
+                                }
+                            default:
+                                {
+                                    Console.WriteLine("csrUNKNOWN");
+                                    break;
+                                }
+                        }
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("UNKNOWN");
+                        break;
+                    }
             }
-            Console.WriteLine();
         }
     }
 }
